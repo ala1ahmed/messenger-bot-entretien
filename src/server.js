6ae31c58afdd0978c6c42ee2b8ccae3da62a182d
ala1ahmed <38598250+ala1ahmed@users.noreply.github.com>
2021-01-30 22:55:46 +0100

    feat: :fire: initiation du projet

---
+++ src/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const api = require("./routes/api");
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use("/api",api);
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Example app listening on port port !`));
