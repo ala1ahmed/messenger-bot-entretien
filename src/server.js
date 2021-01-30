56de4522d870235cf47d12f8d8f7d6b580a64f5f
ala1ahmed <38598250+ala1ahmed@users.noreply.github.com>
2021-01-30 23:00:39 +0100

    make it better

--- src/server.js
+++ src/server.js
@@ -12,6 +12,6 @@ app.get("/", (req, res) => res.send("Hello World!"));
 
 app.use("/api",api);
 
-const port = 5000;
+const port = process.env.PORT ||5000;
 
-app.listen(port, () => console.log(`Example app listening on port port !`));
+app.listen(port, () => console.log(`Example app listening on port ${port} !`));
